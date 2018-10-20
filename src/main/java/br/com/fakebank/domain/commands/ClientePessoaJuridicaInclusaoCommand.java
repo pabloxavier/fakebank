b99f2412fd5aabac54f7730b04b001f294a1348c
Unicred do Brasil - Fabricio Quirino dos Santos <fabricio.santos@e-unicred.com.br>
2018-10-20 11:19:08 -0300

    clientepj-03-Modificacoes nas classes de comando Cliente PJ

--- src/main/java/br/com/fakebank/domain/commands/ClientePessoaJuridicaInclusaoCommand.java
+++ src/main/java/br/com/fakebank/domain/commands/ClientePessoaJuridicaInclusaoCommand.java
@@ -1,5 +1,62 @@
 package br.com.fakebank.domain.commands;
 
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.fakebank.customValidators.CnpjValid;
+
 public class ClientePessoaJuridicaInclusaoCommand {
+	
+	@NotNull
+	@NotBlank
+	@CnpjValid
+	private String cnpj;
+	
+	@NotNull(message = "{clientepj.nome.nao.nulo}")
+	@NotBlank(message = "{clientepj.nome.nao.vazio}")
+	private String nome;
+	
+	private Date dataAbertura;
+	private String enderecoCompleto;
+	
+	public ClientePessoaJuridicaInclusaoCommand() {
+		
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public String getEnderecoCompleto() {
+		return enderecoCompleto;
+	}
+
+	public void setEnderecoCompleto(String enderecoCompleto) {
+		this.enderecoCompleto = enderecoCompleto;
+	}
+	
+	
 
 }
