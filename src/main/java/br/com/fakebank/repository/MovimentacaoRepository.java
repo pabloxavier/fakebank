cd1dcdd266ab5b8af33d142d7ae6af378efb9025
Pablo Tomaz Bonilha Xavier <pablo.xavier@cubotecnologia.com.br>
2018-10-20 11:23:02 -0300

    Revert "Mateus abdala movimentacao"

---
+++ src/main/java/br/com/fakebank/repository/MovimentacaoRepository.java
@@ -0,0 +1,8 @@
+package br.com.fakebank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface MovimentacaoRepository {// extends JpaRepository<Movimentacao, Integer>, JpaSpecificationExecutor<Movimentacao>{
+
+}
