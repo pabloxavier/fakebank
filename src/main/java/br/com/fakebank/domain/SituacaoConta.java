bd099b107298993414f7fd21c8b1ec693bd2c5da
Unicred do Brasil - Thiago La Delfa Cabelleira <thiago.cabelleira@e-unicred.com.br>
2018-10-26 15:38:42 -0300

    ajustes

--- src/main/java/br/com/fakebank/domain/SituacaoConta.java
+++ src/main/java/br/com/fakebank/domain/SituacaoConta.java
@@ -16,7 +16,7 @@ public class SituacaoConta extends Dominio{
 
 	private SituacaoConta(DominioCriacaoCommand comando) {
 
-		this.setTipo(DominioEnum.SIT_CONTA.toString());
+		this.setTipo(DominioEnum.SIT_CONTA.toString()); 
 		this.setValor(comando.getValor());
 		this.setDescricao(comando.getDescricao());
 
