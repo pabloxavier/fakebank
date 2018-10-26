12b1db69c9fd2d642ae633962917ebfd78850fb0
E-UNICRED\juliane.bazilewitz <juliane.bazilewitz@unicred.com.br>
2018-10-26 10:10:10 -0300

    adicionado o Service e Repository para a Movimentacao

--- src/main/java/br/com/fakebank/endpoint/MovimentacaoEndpoint.java
+++ src/main/java/br/com/fakebank/endpoint/MovimentacaoEndpoint.java
@@ -2,29 +2,20 @@ package br.com.fakebank.endpoint;
 
 import java.time.LocalDate;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.fakebank.domain.Agencia;
 import br.com.fakebank.domain.Movimentacao;
-import br.com.fakebank.domain.commands.AgenciaEdicaoCommand;
 import br.com.fakebank.domain.commands.MovimentacaoDepositoCommand;
-import br.com.fakebank.domain.commands.MovimentacaoEdicaoCommand;
-import br.com.fakebank.domain.commands.MovimentacaoInclusaoCommand;
 import br.com.fakebank.domain.commands.MovimentacaoSaqueCommand;
 import br.com.fakebank.domain.commands.MovimentacaoTransferenciaCommand;
-import br.com.fakebank.representations.AgenciaRepresentation;
 import br.com.fakebank.representations.MovimentacaoRepresentation;
 import br.com.fakebank.service.MovimentacaoService;
 
