bc6bc24362f01a98766348ff07a37a49593d6950
E-UNICRED\fabio.couto <fabio.fco@gmail.com>
2018-10-26 12:40:35 -0300

    Criacao dos metodos de salvar contas. Criacao do Specifications de Conta.

--- src/main/java/br/com/fakebank/service/ContaService.java
+++ src/main/java/br/com/fakebank/service/ContaService.java
@@ -3,9 +3,14 @@ package br.com.fakebank.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import br.com.fakebank.domain.Conta;
+import br.com.fakebank.domain.commands.ContaCorrenteInclusaoCommand;
+import br.com.fakebank.domain.commands.ContaPoupancaInclusaoCommand;
+import br.com.fakebank.domain.commands.ContaSalarioInclusaoCommand;
+import br.com.fakebank.domain.specifications.ContaSpecifications;
 import br.com.fakebank.exceptions.NaoEncontradoException;
 import br.com.fakebank.repository.ContaRepository;
 
@@ -19,7 +24,30 @@ public class ContaService {
 		return repository.findAll();
 	}
 	
-	public Conta consultarPorCodigo(Integer codigo) {
-		return repository.findById(codigo).orElseThrow(() -> new NaoEncontradoException());
+	public Conta consultarPorCodigo(String codigo) {
+		Specification<Conta> criteria = Specification.where(ContaSpecifications.porCodigo(codigo));
+		return repository.findOne(criteria).orElseThrow(() -> new NaoEncontradoException());
 	}
+	
+	public List<Conta> filtrarPorTipo(Integer tipoConta){
+		Specification<Conta> criteria = Specification.where(ContaSpecifications.porTipoConta(tipoConta));
+		return repository.findAll(criteria);
+	}
+	
+	public Conta salvarContaCorrente(ContaCorrenteInclusaoCommand command) {
+		Conta contaCorrente = Conta.criarContaCorrente(command);
+		return repository.save(contaCorrente);
+	}
+	
+	public Conta salvarContaPoupanca(ContaPoupancaInclusaoCommand command) {
+		Conta contaPoupanca = Conta.criarContaPoupanca(command);
+		return repository.save(contaPoupanca);
+	}
+	
+	public Conta salvarContaSalario(ContaSalarioInclusaoCommand command) {
+		Conta contaSalario = Conta.criarContaSalario(command);
+		return repository.save(contaSalario);
+	}
+	
+	
 }
