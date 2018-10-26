e536dc2071b95bcc6c0d20bf89559fb4626384d1
mario.almeida <mario.mhba@gmail.com>
2018-10-26 14:48:14 -0300

    endpoint conta

--- src/main/java/br/com/fakebank/endpoint/ContaEndPoint.java
+++ src/main/java/br/com/fakebank/endpoint/ContaEndPoint.java
@@ -1,14 +1,14 @@
 package br.com.fakebank.endpoint;
 
 import br.com.fakebank.domain.Conta;
+import br.com.fakebank.domain.commands.ContaCorrenteInclusaoCommand;
+import br.com.fakebank.domain.commands.ContaPoupancaInclusaoCommand;
+import br.com.fakebank.domain.commands.ContaSalarioInclusaoCommand;
 import br.com.fakebank.representations.ContaRepresentation;
 import br.com.fakebank.service.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("conta")
@@ -21,4 +21,22 @@ public class ContaEndPoint extends FakebankEndpoint {
         Conta conta = contaService.consultarPorCodigo(codigo);
         return ok(ContaRepresentation.from(conta));
     }
+
+    @PostMapping
+    public ResponseEntity<?> incluirContaCorrente(ContaCorrenteInclusaoCommand comando) {
+        contaService.salvarContaCorrente(comando);
+        return created("Conta Corrente incluida com sucesso");
+    }
+    @PostMapping
+    public ResponseEntity<?> incluirContaSalario(ContaSalarioInclusaoCommand comando) {
+        contaService.salvarContaSalario(comando);
+        return created("Conta Salario incluida com sucesso");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> incluirContaPoupanca(ContaPoupancaInclusaoCommand comando) {
+        contaService.salvarContaPoupanca(comando);
+        return created("Conta Poupanca incluida com sucesso");
+    }
+
 }
