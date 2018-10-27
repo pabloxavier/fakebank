d8140ff869e080265ff18b1a897554236266dcd4
Regis Schmitz <regis.schmitz@gmail.com>
2018-10-27 12:16:19 -0300

    adicionado comando de gerenteagencia

---
+++ src/main/java/br/com/fakebank/domain/commands/GerenteAgenciaInclusaoCommand.java
@@ -0,0 +1,41 @@
+package br.com.fakebank.domain.commands;
+
+import br.com.fakebank.domain.Pessoa;
+import br.com.fakebank.domain.validators.CommandValidator;
+import br.com.fakebank.exceptions.FieldName;
+
+import javax.validation.constraints.NotNull;
+
+public class GerenteAgenciaInclusaoCommand {
+
+	// @fieldname -> complementa a mensagem de "@NotNull"
+    @FieldName("codAgencia")
+    @NotNull
+    private Integer codAgencia;
+
+    // @fieldname -> complementa a mensagem de "@NotNull"
+    @FieldName("codGerente")
+    @NotNull
+    private Integer codGerente;
+    
+    private Pessoa  pessoaGerente;
+
+    public GerenteAgenciaInclusaoCommand() {
+
+    }
+
+    public Pessoa getPessoa () {
+    	return pessoaGerente;
+    }
+    public void setPessoa(Pessoa argPessoa) {
+        pessoaGerente = argPessoa;
+    }
+
+    public void setCodAgencia(Integer argCodAgencia) {
+        this.codAgencia = argCodAgencia;
+    }
+
+    public Integer getCodGerente() {
+        return codGerente;
+    }
+}
