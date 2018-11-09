9f26a75c9bae6889eef0bcdd2584b8783b8989d4
Pablo Xavier <pablo.xavier@cubotecnologia.com.br>
2018-11-08 23:22:52 -0200

    Adição de arquivo de configuração de CORS

---
+++ src/main/java/br/com/fakebank/WebConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.fakebank;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			//addMapping recebe um pattern designando os paths aos quais estarão aplicadas
+			//as configurações aqui indicadas.
+			.addMapping("/**")
+			//allowedMethods define a lista de métodos permitidos que uma solicitação pode enviar
+			//Aqui estamos especificando os principais métodos,
+			//mas poderíamos usar "*" para liberar todos os métodos
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+			//allowedHeaders define a lista de headers que uma solicitação pode enviar
+			//no caso, estamos passando "*" para permitir o recebimento de todos os headers
+			.allowedHeaders("*")
+			//magAge indica quantos segundos a resposta de uma request pode ser armazenada pelo Client.
+			//3600 segundos = 1 hora (Valor default = 1800 segundos)
+			.maxAge(3600);
+	}
+}
--- src/main/java/br/com/fakebank/endpoint/AgenciaEndpointV1.java
+++ src/main/java/br/com/fakebank/endpoint/AgenciaEndpointV1.java
@@ -3,7 +3,6 @@ package br.com.fakebank.endpoint;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
