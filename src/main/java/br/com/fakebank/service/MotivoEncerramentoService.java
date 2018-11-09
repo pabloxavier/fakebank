df3116c895c8b0a9effbdc9cb3b7d4c5addb1e8f
cabelleira <thiago.cabelleira@e-unicred.com.br>
2018-11-09 17:47:21 -0200

    validacao unique motivo encerramento

--- src/main/java/br/com/fakebank/service/MotivoEncerramentoService.java
+++ src/main/java/br/com/fakebank/service/MotivoEncerramentoService.java
@@ -5,16 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.fakebank.domain.DominioEnum;
 import br.com.fakebank.domain.MotivoEncerramento;
 import br.com.fakebank.domain.TipoConta;
 import br.com.fakebank.domain.commands.DominioCriacaoCommand;
 import br.com.fakebank.domain.commands.DominioEdicaoCommand;
+import br.com.fakebank.exceptions.DominioUniqueException;
 import br.com.fakebank.exceptions.NotFoundException;
 import br.com.fakebank.repository.MotivoEncerramentoRepository;
 import br.com.fakebank.repository.TipoContaRepository;
 
 @Service
-public class MotivoEncerramentoService {
+public class MotivoEncerramentoService extends DominioService{
 
     @Autowired
     MotivoEncerramentoRepository repository;
@@ -34,6 +36,11 @@ public class MotivoEncerramentoService {
     }
     
     public MotivoEncerramento incluir(DominioCriacaoCommand comando) {
+        
+    	if (dominioExiste(comando.getValor())) {
+			throw new DominioUniqueException();
+		}
+    	
 		MotivoEncerramento motivo = MotivoEncerramento.criar(comando);
 		return repository.save(motivo);
 	}
@@ -46,4 +53,7 @@ public class MotivoEncerramentoService {
 		return repository.save(motivo);
 	}
     
+    private boolean dominioExiste(String valor){
+    	return dominioExiste(valor, DominioEnum.MOTIVO.toString());
+    }
 }
