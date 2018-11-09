4bfd7cfbc307b1cb8a332b31aba56e3dc6088a0c
Luis <luisalberto677@gmail.com>
2018-11-09 12:11:35 -0200

    ajuste encerramento

--- src/main/java/br/com/fakebank/repository/ContaEncerradaRepository.java
+++ src/main/java/br/com/fakebank/repository/ContaEncerradaRepository.java
@@ -1,7 +1,8 @@
 package br.com.fakebank.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import src.main.java.br.com.fakebank.domain.ContaEncerrada;
+import br.com.fakebank.domain.ContaEncerrada;
 
 public interface ContaEncerradaRepository extends JpaRepository<ContaEncerrada, Integer>, 
 										  JpaSpecificationExecutor<ContaEncerrada>{
