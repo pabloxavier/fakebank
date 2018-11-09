95323c6ef63ef83a1c6cdbe28243e6e039add814
Unicred do Brasil - Marcio Silveira da Rocha <marcio.rocha@e-unicred.com.br>
2018-11-09 08:57:42 -0300

    implementação de de endpoit e service motivo encerramento

--- src/main/java/br/com/fakebank/endpoint/MotivoEncerramentoEndpoint.java
+++ src/main/java/br/com/fakebank/endpoint/MotivoEncerramentoEndpoint.java
@@ -18,32 +18,33 @@ import br.com.fakebank.service.MotivoEncerramentoService;
 
 @RestController
 @RequestMapping("motivo-encerramento")
-public class MotivoEncerramentoEndpoint extends FakebankEndpoint{
+public class MotivoEncerramentoEndpoint extends FakebankEndpoint {
 
-    @Autowired
-    MotivoEncerramentoService service;
-    
-    @GetMapping
-    public ResponseEntity<?> listarTipoConta(){
-        
-        return ok(service.listar());
-    }
-    
-    @GetMapping(value="/{codigo}")
-    public ResponseEntity<?> cosultaTipoContaPorCodigo(@PathVariable("codigo") Integer codigo ){
-        MotivoEncerramento motivo = service.consultaPorCodigo(codigo);
-        return ok(motivo);
-    }
-    
-    @PostMapping
+	@Autowired
+	MotivoEncerramentoService service;
+
+	@GetMapping
+	public ResponseEntity<?> listarTipoConta() {
+
+		return ok(service.listar());
+	}
+
+	@GetMapping(value = "/{codigo}")
+	public ResponseEntity<?> cosultaTipoContaPorCodigo(@PathVariable("codigo") Integer codigo) {
+		MotivoEncerramento motivo = service.consultaPorCodigo(codigo);
+		return ok(motivo);
+	}
+
+	@PostMapping
 	public ResponseEntity<?> incluirMotivoEncerramento(@RequestBody DominioCriacaoCommand comando) {
 		service.incluir(comando);
 		return new ResponseEntity<>("incluido com sucesso", HttpStatus.CREATED);
 	}
- 	@PutMapping(path = "/{codigo}")
+
+	@PutMapping(path = "/{codigo}")
 	public ResponseEntity<?> editarMotivoEncerramento(@PathVariable("codigo") Integer codigo,
 			@RequestBody DominioEdicaoCommand comando) {
- 		service.editar(comando, codigo);
+		service.editar(comando, codigo);
 		return new ResponseEntity<>("Iditado com sucesso", HttpStatus.OK);
- 	}
+	}
 }
