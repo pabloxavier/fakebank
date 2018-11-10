8350a1fa27d5d61dba24a736629e93ae89302259
Pablo Xavier <pablo.xavier@cubotecnologia.com.br>
2018-11-10 14:22:16 -0200

    Implementações para funcionamento do Swagger

--- src/main/java/br/com/fakebank/SwaggerConfiguration.java
+++ src/main/java/br/com/fakebank/SwaggerConfiguration.java
@@ -28,48 +28,47 @@ import com.google.common.collect.Lists;
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfiguration {
 
-	@Bean
-	public Docket apiDoc() {
-		
-		List<ResponseMessage> defaultResponseMessages =
-	  		Lists.newArrayList(
-				new ResponseMessageBuilder()
-					.code(401)
-					.message("Recurso não autorizado para acesso")
-					.build(),
-				new ResponseMessageBuilder()
-					.code(403)
-					.message("Acesso negado")
-					.build(),
-				new ResponseMessageBuilder()
-					.code(500)
-					.message("Failed to process request")
-					.build());
-				
-		
-		return new Docket(DocumentationType.SWAGGER_2)
-			.select()
-			.apis(RequestHandlerSelectors.any())
-			.paths(Predicates.not(PathSelectors.regex("/error")))
-			.build()
-			.apiInfo(metaData())
-			.globalResponseMessage(RequestMethod.GET, defaultResponseMessages)
-			.globalResponseMessage(RequestMethod.POST, defaultResponseMessages)
-			.globalResponseMessage(RequestMethod.PUT, defaultResponseMessages)
-			.globalResponseMessage(RequestMethod.DELETE, defaultResponseMessages);
-	}
-	
-	private ApiInfo metaData() {
+    @Bean
+    public Docket apiDoc() {
+    
+        List<ResponseMessage> defaultResponseMessages =
+            Lists.newArrayList(
+                new ResponseMessageBuilder()
+                    .code(401)
+                    .message("Recurso não autorizado para acesso")
+                    .build(),
+                new ResponseMessageBuilder()
+                    .code(403)
+                    .message("Acesso negado")
+                    .build(),
+                new ResponseMessageBuilder()
+                    .code(500)
+                    .message("Failed to process request")
+                    .build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(Predicates.not(PathSelectors.regex("/error")))
+            .build()
+            .apiInfo(metaData())
+            .globalResponseMessage(RequestMethod.GET, defaultResponseMessages)
+            .globalResponseMessage(RequestMethod.POST, defaultResponseMessages)
+            .globalResponseMessage(RequestMethod.PUT, defaultResponseMessages)
+            .globalResponseMessage(RequestMethod.DELETE, defaultResponseMessages);
+    }
+    
+    private ApiInfo metaData() {
         ApiInfo informacoesApiSwagger = new ApiInfo(
-                "API do Fakebank",
-                "Fakebank API para consumo da aplicação.",
-                "1.0",
-                "Termos de serviço",
-                new Contact("Equipe UC", "", ""),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licenses/LICENSE-2.0",
-                new ArrayList<VendorExtension>());
-        
+            "API do Fakebank",
+            "Fakebank API para consumo da aplicação.",
+            "1.0",
+            "Termos de serviço",
+            new Contact("Equipe UC", "", ""),
+            "Apache License Version 2.0",
+            "https://www.apache.org/licenses/LICENSE-2.0",
+            new ArrayList<VendorExtension>());
+            
         return informacoesApiSwagger;
     }
 }
