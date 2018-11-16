c046048c778c7548cff95977b0299fbe4523cf88
Alan Oliveira <alan.oliveira@unicred.com.br>
2018-11-16 17:09:54 -0200

    Arruma problema de compilação no serviço de cliente

--- src/main/java/br/com/fakebank/service/ClienteService.java
+++ src/main/java/br/com/fakebank/service/ClienteService.java
@@ -130,16 +130,15 @@ public class ClienteService {
 	}
 
 	public ClienteTelefone salvarTelefone(Cliente cliente, ClienteTelefoneInclusaoCommand comando) {
-		ClienteTelefone telefone = ClienteTelefone.criar(cliente, comando,
-				getProximoCodigoTelefoneFromCliente(cliente.getCodigo()));
+		ClienteTelefone telefone = ClienteTelefone.criar(cliente, comando, getProximoCodigoTelefoneFromCliente(cliente.getCodigo()));
 		return telefoneRepository.save(telefone);
 	}
 
-	public List<ClienteTelefone> listarTelefonesFromCliente(Integer codigo) {
-		Specification<ClienteTelefone> criterio = Specification
-				.where(ClienteTelefoneSpecifications.porCodigoCliente(codigo));
+	public Page<ClienteTelefone> listarTelefonesFromCliente(Integer codigo, Pageable pageable) {
+		Specification<ClienteTelefone> criterio =
+				Specification.where(ClienteTelefoneSpecifications.porCodigoCliente(codigo));
 
-		return telefoneRepository.findAll(criterio);
+		return telefoneRepository.findAll(criterio, pageable);
 	}
 
 	private ClienteTelefone getTelefoneById(ClienteTelefoneId clienteTelefoneId) {
@@ -149,112 +148,23 @@ public class ClienteService {
 	public boolean excluirTelefone(Integer codigoCliente, Short codigoTelefone) {
 		ClienteTelefone telefone = getTelefoneById(new ClienteTelefoneId(codigoCliente, codigoTelefone));
 
-		if (telefone == null)
+		if(telefone == null)
 			return false;
 
 		telefoneRepository.delete(telefone);
 		return true;
 	}
 
-	public ClienteTelefone salvarTelefone(Integer codigoCliente, Short codigoTelefone,
-			ClienteTelefoneEdicaoCommand comando) {
+
+	public ClienteTelefone salvarTelefone(Integer codigoCliente, Short codigoTelefone, ClienteTelefoneEdicaoCommand comando) {
 		ClienteTelefone telefone = getTelefoneById(new ClienteTelefoneId(codigoCliente, codigoTelefone));
 
-		if (telefone == null)
+		if(telefone == null)
 			return telefone;
 
 		telefone.editar(comando);
 		return telefoneRepository.save(telefone);
 	}
-
-    @Autowired
-    private ClienteRepository repository;
-
-    @Autowired
-    private ClienteTelefoneRepository telefoneRepository;
-    
-    public List<Cliente> listar(){
-        return repository.findAll();
-    }
-    
-    public Cliente getClienteById(Integer codigo){
-        return repository.findById(codigo).orElse(null);
-    }
-
-    public List<Cliente> filtrar(String endereco, boolean isAtivo){
-        Specification<Cliente> criterio = Specification.where(ClienteSpecifications.porParteEndereco(endereco)
-                                                       .and(ClienteSpecifications.porSituacao(isAtivo)));
-        return repository.findAll(criterio);    
-    }
-    
-    public Cliente salvar(ClienteInclusaoCommand comando){
-        Cliente cliente = Cliente.criar(comando);
-        return repository.save(cliente);
-    }
-    
-    public Cliente salvar(Integer codigo, ClienteEdicaoCommand comando){
-        Cliente cliente = getClienteById(codigo);
-        
-        if (cliente == null) 
-            return cliente;
-
-        
-        cliente.editar(comando);
-        return repository.save(cliente);
-    }
-    
-    public boolean excluir(Integer codigo){
-        Cliente cliente = getClienteById(codigo);
-        
-        if (cliente == null)
-            return false;
-        
-        repository.deleteById(codigo);
-        return true;
-    }
-
-    private Short getProximoCodigoTelefoneFromCliente(Integer codigoCliente) {
-        Short codigoTelefone = telefoneRepository.getUltimoCodigoTelefoneFromCliente(codigoCliente);
-
-        return codigoTelefone == null ? (short) 1 : (short) (codigoTelefone + 1);
-    }
-
-    public ClienteTelefone salvarTelefone(Cliente cliente, ClienteTelefoneInclusaoCommand comando) {
-        ClienteTelefone telefone = ClienteTelefone.criar(cliente, comando, getProximoCodigoTelefoneFromCliente(cliente.getCodigo()));
-        return telefoneRepository.save(telefone);
-    }
-
-    public Page<ClienteTelefone> listarTelefonesFromCliente(Integer codigo, Pageable pageable) {
-        Specification<ClienteTelefone> criterio =
-                Specification.where(ClienteTelefoneSpecifications.porCodigoCliente(codigo));
-
-        return telefoneRepository.findAll(criterio, pageable);
-    }
-
-    private ClienteTelefone getTelefoneById(ClienteTelefoneId clienteTelefoneId) {
-        return telefoneRepository.findById(clienteTelefoneId).orElse(null);
-    }
-
-    public boolean excluirTelefone(Integer codigoCliente, Short codigoTelefone) {
-        ClienteTelefone telefone = getTelefoneById(new ClienteTelefoneId(codigoCliente, codigoTelefone));
-
-        if(telefone == null)
-            return false;
-
-        telefoneRepository.delete(telefone);
-        return true;
-    }
-
-
-    public ClienteTelefone salvarTelefone(Integer codigoCliente, Short codigoTelefone, ClienteTelefoneEdicaoCommand comando) {
-    	ClienteTelefone telefone = getTelefoneById(new ClienteTelefoneId(codigoCliente, codigoTelefone));
-    	
-    	if(telefone == null)
-    		return telefone;
-    	
-    	telefone.editar(comando);
-    		return telefoneRepository.save(telefone);
-    }
     
 
 }
