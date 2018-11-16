2e8ee11cb1b7931109dcaaddafbe6ca1c61c5944
Gabriel Correia Gonçalves <gabriel.correia@unicred.com.br>
2018-11-16 12:19:41 -0200

    qweqweqwe

--- src/main/java/br/com/fakebank/service/GerenteService.java
+++ src/main/java/br/com/fakebank/service/GerenteService.java
@@ -3,6 +3,8 @@ package br.com.fakebank.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import br.com.fakebank.domain.Gerente;
 import br.com.fakebank.domain.commands.GerenteEdicaoCommand;
 import br.com.fakebank.domain.commands.GerenteInclusaoCommand;
 import br.com.fakebank.domain.specifications.GerenteSpecifications;
+import br.com.fakebank.exceptions.NotFoundException;
 import br.com.fakebank.repository.GerenteRepository;
 
 @Service
@@ -18,8 +21,12 @@ public class GerenteService {
     @Autowired
     private GerenteRepository repository;
 
-    public List<Gerente> listar() {
-        return repository.findAll();
+    public Page<Gerente> listar(Pageable pageable){
+    	Page<Gerente> gerentes = repository.findAll(pageable);
+    	
+    	if (gerentes.getSize() == 0) throw new NotFoundException();
+    	
+        return gerentes;
     }
     
     public Gerente getGerenteById(Integer codigo) {
