d48ba181003452ffa15bd80e427d1bb3ba973f59
Unicred do Brasil - Caetano Yunes Vieira <caetano.vieira@e-unicred.com.br>
2018-11-17 10:47:49 -0200

    Ajustes dos campos de data para LocalDate. Melhorias nos retornos dos endpoints de clinte

--- src/main/java/br/com/fakebank/endpoint/ClientePessoaJuridicaEndpointV1.java
+++ src/main/java/br/com/fakebank/endpoint/ClientePessoaJuridicaEndpointV1.java
@@ -33,10 +33,10 @@ import io.swagger.annotations.ApiResponses;
 public class ClientePessoaJuridicaEndpointV1 extends FakebankEndpoint{
 
     @Autowired
-    ClienteService service;
+    private ClienteService service;
     
     @Autowired
-    ClientePessoaJuridicaService servicePJ;
+    private ClientePessoaJuridicaService servicePJ;
     
     @ApiOperation(
             value = "Listar todos os clientes PJ cadastrados.",
@@ -45,7 +45,7 @@ public class ClientePessoaJuridicaEndpointV1 extends FakebankEndpoint{
             @ApiResponse(code = 200, message = "Clientes PJ retornadas com sucesso."),
             @ApiResponse(code = 401, message = "Recurso sem autorização de acesso."),
             @ApiResponse(code = 403, message = "Acesso negado ao recurso."),
-            @ApiResponse(code = 404, message = "Nenhum cliente PJ encontrada.")
+            @ApiResponse(code = 404, message = "Nenhum cliente PJ encontrado.")
     })
     @GetMapping
     public ResponseEntity<?> listarClientes(Pageable pageable){
@@ -54,9 +54,21 @@ public class ClientePessoaJuridicaEndpointV1 extends FakebankEndpoint{
         return ok(model);        
     }
     
+    @ApiOperation(
+    		value = "Consultar um único cliente PJ por código.",
+    		response = ClienteRepresentationV1.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Cliente PJ retornado com sucesso."),
+            @ApiResponse(code = 401, message = "Recurso sem autorização de acesso"),
+            @ApiResponse(code = 403, message = "Acesso negado ao recurso"),
+            @ApiResponse(code = 404, message = "Nenhuma cliente PJ encontrado")
+    })
     @GetMapping(value = "/{codigo}")
     public ResponseEntity<?> getClienteById(@PathVariable("codigo") Integer codigo){
-        return ok(service.getClienteById(codigo));
+        Cliente cliente = service.getClienteById(codigo);
+        
+        ClienteRepresentationV1 model = ClienteRepresentationV1.from(cliente);
+        return ok(model);        
     }
        
     @GetMapping(path = "/pesquisa")
@@ -64,28 +76,54 @@ public class ClientePessoaJuridicaEndpointV1 extends FakebankEndpoint{
                                      @RequestParam(name = "isAtivo") boolean isAtivo){
         return ok(service.filtrar(endereco, isAtivo));
     }
-        
+
+    @ApiOperation(
+    		value = "Incluir um novo cliente PJ.",
+    		response = ClientePessoaJuridicaInclusaoCommand.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Cliente PJ criado com sucesso."),
+            @ApiResponse(code = 400, message = "Inclusão não permitida por validações.")
+    })
     @PostMapping
     public ResponseEntity<?> criar(@RequestBody ClientePessoaJuridicaInclusaoCommand comando){
         Cliente cliente = servicePJ.salvar(comando);
-        if (cliente != null)
-            return created("Cliente PJ incluido com sucesso");
-        else
-            return notFound("erro ao incluir cliente PJ");
+
+        ClienteRepresentationV1 model = ClienteRepresentationV1.from(cliente);
+        return created(model, cliente.getCodigo());
     }
     
+    @ApiOperation(
+    		value = "Alterar informações de um cliente PJ.",
+    		response = ClientePessoaJuridicaEdicaoCommand.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Cliente PJ alterado com sucesso."),
+            @ApiResponse(code = 400, message = "Alteração não permitida por validações."),
+            @ApiResponse(code = 404, message = "Cliente PJ não encontrado")
+    })
     @PutMapping(value = "/{codigo}")
-    public ResponseEntity<?> editar(@PathVariable("codigo") Integer codigo, @RequestBody ClientePessoaJuridicaEdicaoCommand comando){
+    public ResponseEntity<?> editar(
+    		@PathVariable("codigo") Integer codigo, 
+    		@RequestBody ClientePessoaJuridicaEdicaoCommand comando){
         Cliente cliente = servicePJ.salvar(codigo, comando);
-        if (cliente != null)
-            return created("Cliente PJ editado com sucesso");
-        else
-            return notFound("Cliente PJ não encontrado");
+        
+        ClienteRepresentationV1 model = ClienteRepresentationV1.from(cliente);
+        return ok(model);
     }
       
+    @ApiOperation(
+    		value = "Excluir um cliente PJ por código.",
+    		response = ClienteRepresentationV1.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Cliente PJ removido com sucesso."),
+            @ApiResponse(code = 400, message = "Exclusão não permitida por validações."),
+            @ApiResponse(code = 404, message = "Cliente PJ não encontrado")
+    })
     @DeleteMapping(value = "/{codigo}")
-    public ResponseEntity<?> excluirCliente(@PathVariable("codigo") Integer codigo){
-        return service.excluir(codigo) ? ok("Cliente PJ excluído com sucesso") : notFound("Cliente PJ não encontrado");
+    public ResponseEntity<?> excluirCliente(
+    		@PathVariable("codigo") Integer codigo){
+    	
+        service.excluir(codigo);
+        return ok();
     }
     
 }
